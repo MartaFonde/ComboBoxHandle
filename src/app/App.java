8f425445d44121327516ab2bf12a8936530ef658
Marta Fondevila <martafondevila8@gmail.com>
2020-05-13 19:41:21 +0200

    Prueba2

--- src/app/App.java
+++ src/app/App.java
@@ -4,8 +4,8 @@ import javax.swing.JFrame;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        Prog p = new Prog();
-        p.setSize(800,600);
+        SwingDelCombo p = new SwingDelCombo();
+        p.setSize(800,450);
         p.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
         p.setVisible(true);
     }
--- src/app/SwingDelCombo.java
+++ src/app/SwingDelCombo.java
@@ -1,16 +1,14 @@
 package app;
 
 import javax.swing.*;
-
 import java.awt.Color;
 import java.awt.event.*;
 
-/**
- * Prog
- */
-public class Prog extends JFrame implements ItemListener {
-    private JComboBox cbA;
-    private JComboBox cbB;
+public class SwingDelCombo extends JFrame implements ActionListener, ItemListener {
+    private JComboBox<String> cbA;
+    private String[] nombreDirector = {"David", "Lars", "Andréi", "Sofia", "Darren"};
+    private JComboBox<String> cbB;
+    private String[] apellidoDirector = {"Lynch", "Von Trier", "Tarkovski", "Coppola", "Aronofsky"};
     private JButton btnAñadir;
     private JButton btnQuitar;
     private JButton btnTraspasarAB;
@@ -19,26 +17,24 @@ public class Prog extends JFrame implements ItemListener {
     private JTextField txf2;
     private JLabel lblElem;
     private JLabel lblInd;
-    Timer temporizador;
+    private Timer temporizador;
     private JLabel seg;
-    int cont;
+    private int cont;
 
-    public Prog() {
+    public SwingDelCombo() {
         super("ComboBox");
         setLayout(null);
 
-        cbA = new JComboBox();
+        cbA = new JComboBox<String>(nombreDirector);
         cbA.setSize(160, 25);
         cbA.setLocation(15, 15);
         cbA.setToolTipText("ComboBox A");
-        cbA.addItem("David");
         cbA.addItemListener(this);
         add(cbA);
 
-        cbB = new JComboBox();
+        cbB = new JComboBox<String>(apellidoDirector);
         cbB.setSize(160, 25);
         cbB.setLocation(230, 15);
-        cbB.addItem("Lynch");
         cbB.setToolTipText("Índice seleccionado de ComboBox B: " + cbB.getSelectedIndex());
         cbB.addItemListener(this);
         add(cbB);
@@ -47,34 +43,34 @@ public class Prog extends JFrame implements ItemListener {
         btnTraspasarAB.setSize(btnTraspasarAB.getPreferredSize());
         btnTraspasarAB.setLocation(430, 10);
         btnTraspasarAB.setToolTipText("Traspasa el valor seleccionado de la comboBox A a la B");
-        btnTraspasarAB.addMouseListener(new MouseHandler());
+        btnTraspasarAB.addActionListener(this);
         add(btnTraspasarAB);
 
         btnTraspasarBA = new JButton("Transpasar B -> A");
         btnTraspasarBA.setSize(btnTraspasarBA.getPreferredSize());
         btnTraspasarBA.setLocation(600, 10);
         btnTraspasarBA.setToolTipText("Traspasa el valor seleccionado de la comboBox B a la A");
-        btnTraspasarBA.addMouseListener(new MouseHandler());
+        btnTraspasarBA.addActionListener(this);
         add(btnTraspasarBA);
 
         txf1 = new JTextField(20);
         txf1.setSize(txf1.getPreferredSize());
         txf1.setLocation(10, 55);
         txf1.setToolTipText(
-                "Items a añadir a ComboBox A. Se pueden introducir varias entradas separadas por punto y coma(;). No admite entradas vacías. Pulsa Añadir.");
+                "Items a añadir a ComboBox A. Introduce varias entradas separadas ; Pulsa Añadir");
         add(txf1);
 
         btnAñadir = new JButton("Añadir");
         btnAñadir.setSize(btnAñadir.getPreferredSize());
         btnAñadir.setLocation(250, 50);
-        btnAñadir.setToolTipText("Añadir a ComboBox A los items introducidos en el campo de texto");
-        btnAñadir.addMouseListener(new MouseHandler());
+        btnAñadir.setToolTipText("Añade a ComboBox A los items introducidos en el campo de texto");
+        btnAñadir.addActionListener(this);
         add(btnAñadir);
 
         txf2 = new JTextField(20);
         txf2.setSize(txf2.getPreferredSize());
         txf2.setLocation(10, 105);
-        txf2.setToolTipText("Items a borrar de ComboBox A que comiencen por el texto aquí introducido. Pulsa Quitar.");
+        txf2.setToolTipText("Items a borrar de ComboBox A que comiencen por el texto aquí introducido. Pulsa Quitar");
         add(txf2);
 
         btnQuitar = new JButton("Quitar");
@@ -82,7 +78,8 @@ public class Prog extends JFrame implements ItemListener {
         btnQuitar.setLocation(250, 100);
         btnQuitar.setBackground(Color.white);
         btnQuitar.setToolTipText(
-                "Borra de ComboBox A el item seleccionado o los que comiencen por lo introducido en el campo de texto.");
+                "Borra de ComboBox A el item seleccionado o los que comiencen por lo introducido en el campo de texto");
+        btnQuitar.addActionListener(this);
         btnQuitar.addMouseListener(new MouseHandler());
         add(btnQuitar);
 
@@ -118,11 +115,55 @@ public class Prog extends JFrame implements ItemListener {
                     txf1.setText("");
                     txf2.setText("");
                     cont = 0;
+                    lblElem.setText("Cantidad de elementos en cbA: " + cbA.getItemCount());
+                    lblInd.setText("Índice seleccionado en cbA: " + cbA.getSelectedIndex());            
                 }
             }
         });
         temporizador.start();
+    }
 
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == btnAñadir) {
+            if (txf1.getText().trim().length() > 0) {
+                if (txf1.getText().contains(";")) {
+                    String[] items = txf1.getText().split(";");
+                    for (String item : items) {
+                        if (item.trim().length() != 0) {
+                            cbA.addItem(item.trim());
+                        }
+                    }
+                } else {
+                    cbA.addItem(txf1.getText().trim());
+                }
+            }
+        }
+
+        if (e.getSource() == btnQuitar) {
+            if (txf2.getText().length() == 0) {
+                cbA.removeItemAt(cbA.getSelectedIndex());
+            } else {
+                for (int i = cbA.getItemCount() - 1 ; i >= 0; i--) {
+                    if (cbA.getItemAt(i).startsWith(txf2.getText())) {
+                        cbA.removeItemAt(i);
+                    }
+                }
+            }
+        }
+
+        if (e.getSource() == btnTraspasarAB) {
+            cbB.addItem(cbA.getSelectedItem().toString());
+            cbA.removeItem(cbA.getSelectedItem());
+        }
+
+        if (e.getSource() == btnTraspasarBA) {
+            cbA.addItem(cbB.getSelectedItem().toString());
+            cbB.removeItem(cbB.getSelectedItem());
+        }
+        lblElem.setText("Cantidad de elementos en cbA: " + cbA.getItemCount());
+        lblInd.setText("Índice seleccionado en cbA: " + cbA.getSelectedIndex());
+        cont = 0;
     }
 
     @Override
@@ -135,47 +176,6 @@ public class Prog extends JFrame implements ItemListener {
     }
 
     private class MouseHandler extends MouseAdapter {
-        @Override
-        public void mouseClicked(java.awt.event.MouseEvent e) {
-            if (e.getSource() == btnAñadir) {
-                if (txf1.getText().trim().length() > 0) {
-                    if (txf1.getText().contains(";")) {
-                        String[] items = txf1.getText().split(";");
-                        for (String item : items) {
-                            if (item.trim().length() != 0) {
-                                cbA.addItem(item.trim());
-                            }
-                        }
-                    } else {
-                        cbA.addItem(txf1.getText().trim());
-                    }
-                }
-            }
-
-            if (e.getSource() == btnQuitar) {
-                if (txf2.getText() == "") {
-                    cbA.removeItem(cbA.getSelectedItem());
-                } else {
-                    for (int i = 0; i < cbA.getItemCount(); i++) {
-                        if (cbA.getItemAt(i).toString().startsWith(txf2.getText())) {
-                            cbA.removeItemAt(i);
-                        }
-                    }
-                }
-            }
-
-            if (e.getSource() == btnTraspasarAB) {
-                cbB.addItem(cbA.getSelectedItem());
-            }
-
-            if (e.getSource() == btnTraspasarBA) {
-                cbA.addItem(cbB.getSelectedItem());
-            }
-
-            lblElem.setText("Cantidad de elementos en cbA: " + cbA.getItemCount());
-            cont = 0;
-        }
-
         @Override
         public void mouseEntered(java.awt.event.MouseEvent e) {
             if (e.getSource() == btnQuitar) {
@@ -202,4 +202,5 @@ public class Prog extends JFrame implements ItemListener {
             }
         }
     }
+
 }
